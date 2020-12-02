3b865bc5052e7c2c08d9723302b3afa3383ab6ab
chxlay <chxlay@gmail.com>
2020-12-02 10:29:38 +0800

    模块调整

---
+++ time-server/src/main/java/com/chxlay/timeserver/TimeServerApplication.java
@@ -0,0 +1,18 @@
+package com.chxlay.timeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Alay
+ * @date 2020-12-02 10.01
+ * @project netty-chat
+ */
+@SpringBootApplication
+public class TimeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeServerApplication.class, args);
+    }
+
+}
