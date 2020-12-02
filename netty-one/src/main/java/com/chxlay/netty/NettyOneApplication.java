b8cdbb16d925b06f1e6647aff1c9771da3360633
chxlay <chxlay@gmail.com>
2020-12-02 09:59:17 +0800

    新建学习模块一

---
+++ netty-one/src/main/java/com/chxlay/netty/NettyOneApplication.java
@@ -0,0 +1,13 @@
+package com.chxlay.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyOneApplication.class, args);
+    }
+
+}
