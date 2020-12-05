28bbd3cb2aa9ae670f9bcda7247cc410d50a9686
chxlay <chxlay@gmail.com>
2020-12-05 17:49:58 +0800

    格式化

--- netty-chat/src/main/java/com/chxlay/chat/initializer/ChatInitializer.java
+++ netty-chat/src/main/java/com/chxlay/chat/initializer/ChatInitializer.java
@@ -6,7 +6,6 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -28,16 +27,14 @@ public class ChatInitializer extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel socketChannel) {
 
         socketChannel.pipeline()
-                // 将一个请求，应答的消息编码，解码为一个Http 的消息
-                .addLast(new HttpServerCodec())
-                // 向客户端发送 HTML5 的文件
-                .addLast(new ChunkedWriteHandler())
-                // 将多条信息整合为一条,将一个Http消息尽心聚合
+                // 对写大数据流的支持
+                .addLast("chunkedWriteHandler", new ChunkedWriteHandler())
+                // 将多条信息整合为一条,将一个Http消息尽心聚合,设置单次请求的文件大小上限
                 .addLast(new HttpObjectAggregator(8192))
                 // HttpRequestDecoder 和 HttpResponseEncoder 的组合,处理解码编码
                 .addLast("httpServerCodec", new HttpServerCodec())
                 // 创建握手操作
-                .addLast(new WebSocketServerProtocolHandler("/websocket"))
+                // .addLast(new WebSocketServerProtocolHandler("/websocket"))
                 // 自定义的握手操作
                 .addLast(webSocketHandler)
 
